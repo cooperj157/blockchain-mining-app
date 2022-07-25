b73f5b35e85154962dee6d275a9c42177dfc62d6
cooperj157 <cooperj157@gmail.com>
2022-07-25 19:37:14 -0400

    mining functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import { useState } from "react";
 import BlockComponent from './blockComponent'
 import Block from './block';
 import BigInt from 'big-integer';
+import SHA256 from 'crypto-js/sha256';
+
+let IDindex = 0;
+const target = BigInt("000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",16);
+
+
+
 function App() {
-
-  const target = BigInt("000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",16);
-
-  let IDindex = 0;
   const [chain, addBlock] = useState([[IDindex,null,null,null,null]]);
-
+  
   function mineBlock(){
     IDindex++;
 
@@ -16,8 +19,22 @@ function App() {
 
     const currBlock = new Block(IDindex,data);
     
-    mineBlock(currBlock,target)
-    addBlock([...chain,[IDindex,data,null,null,null]])
+    currBlock.prevHash = "n/a";
+
+    if(IDindex > 1){
+      const lastBlockIndex = chain[IDindex-1];
+      let lastBlock = new Block(lastBlockIndex[0],lastBlockIndex[1]);
+      lastBlock.prevHash = lastBlockIndex[2];
+      lastBlock.nonce = lastBlockIndex[4];
+      
+      // set current block prevHash to last block's hash
+      currBlock.prevHash = toHash(lastBlock);
+    }
+    
+    mine(currBlock,target);
+
+    addBlock([...chain,[IDindex,data,currBlock.prevHash,currBlock.hash,currBlock.nonce]])
+    
   }
   
   return (
@@ -27,8 +44,6 @@ function App() {
       </button>
       {chain.map((block, i) => (<BlockComponent key={i} block={block}/> ))}
     </div>
-    
-   
     
   );
 }
@@ -40,11 +55,26 @@ function createRandomTx(){
   const addresses = ['A','B','C','D','E'];
   let randA = addresses[Math.floor(Math.random()*addresses.length)];
   addresses.splice(addresses.indexOf(randA),1);
-  console.log(randA,addresses);
   let randB = addresses[Math.floor(Math.random()*addresses.length)];
   let randNum = Math.floor(Math.random()*1000);
   return(`${randA} pays ${randB} ${randNum} dollars`);
 }
 
+function mine(block,targetDiff){
+  block.nonce = 0;
+  while(true){
+    let hash = SHA256(JSON.stringify(block)).toString();
+      if(BigInt(hash,16) < targetDiff){
+        block.hash = hash;
+        break;
+      }
+      block.nonce++;
+  }
+}
+
+//A toHash function which will create a hash of this block using the data it contains
+function toHash(block){
+  return SHA256(JSON.stringify(block)).toString();
+}
 
 export default App;
--- src/block.js
+++ src/block.js
@@ -7,10 +7,7 @@ class Block {
        
     }
 
-    //A toHash function which will create a hash of this block using the data it contains
-    toHash(){
-        return SHA256(this.id+this.previousHash+this.data+this.nonce).toString();
-    }
+    
 }
 
 //this line makes it so the other JS files in the folder can access (this comment is for me)
